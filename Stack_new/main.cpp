61d223acae8ec91d3266e1d719c2e8856e2418fc
Daniel <danila.dudin@mail.ru>
2020-02-19 13:46:38 +0300

    Initial Commit

---
+++ Stack_new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack_new
+//
+//  Created by Данила Дудин on 19.02.2020.
+//  Copyright © 2020 Данила Дудин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
